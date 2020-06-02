7ad1046eb0e946e2608264fd591dc60a40a7b210
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-06-02 11:18:20 -0400

    updated files

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+        Invoke(nameof(LoadFirstScene), 2f);
+    }
+
+    private void LoadFirstScene() {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
