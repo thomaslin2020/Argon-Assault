1d0141194015843bdf850c9432cefec1ae2be8e6
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-06-02 12:53:41 -0400

    updated files

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
-{
-    void Awake() {
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
         DontDestroyOnLoad(gameObject);
-    }
-
-    private void Start() {
-        Invoke(nameof(LoadFirstScene), 2f);
-    }
-
-    private void LoadFirstScene() {
-        SceneManager.LoadScene(1);
-    }
-
-    // Update is called once per frame
-    void Update() {
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,59 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
-    [Header("Speed")] [Tooltip("In ms^-1")] [SerializeField]
-    private float xSpeed = 20f;
 
-    [Tooltip("In ms^-1")] [SerializeField] private float ySpeed = 20f;
-    [SerializeField] private float positionPitchFactor = -5f;
-    [SerializeField] private float controlPitchFactor = -20f;
-    [SerializeField] private float positionYawFactor = 5f;
-    [SerializeField] private float controlRollFactor = -20f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
 
-    [Header("Range")] [Tooltip("In m")] [SerializeField]
-    private float xRange = 4f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
 
-    [Tooltip("In m")] [SerializeField] private float yRangeMin = -3f;
-    [Tooltip("In m")] [SerializeField] private float yRangeMax = 3f;
+    float xThrow, yThrow;
 
-    private float _xThrow;
-    private float _yThrow;
-
-    // Start is called before the first frame update
-    void Start() {
-    }
-
-    // Update is called once per frame
-    void Update() {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
         ProcessTranslation();
         ProcessRotation();
     }
 
-    private void ProcessRotation() {
-        float pitch = transform.localPosition.y * positionPitchFactor + _yThrow * controlPitchFactor;
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
         float yaw = transform.localPosition.x * positionYawFactor;
-        float roll = _xThrow * controlRollFactor;
+
+        float roll = xThrow * controlRollFactor;
+
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
-    private void ProcessTranslation() {
-        _xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        _yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
-        float xOffset = _xThrow * xSpeed * Time.deltaTime;
-        float yOffset = _yThrow * ySpeed * Time.deltaTime;
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
 
-        var position = transform.localPosition;
-
-        float rawXPos = position.x + xOffset;
-        float rawYPos = position.y + yOffset;
-
+        float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-        float clampedYPos = Mathf.Clamp(rawYPos, yRangeMin, yRangeMax);
 
-        transform.localPosition = new Vector3(clampedXPos, clampedYPos, position.z);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
